1cc4b064404fd56ca8c01fbfd51b7568be09e05e
Lucas <lucas.gorgueira@gmail.com>
2025-12-05 00:30:51 -0300

    User update scenario added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -3,21 +3,42 @@ import userData from '../fixtures/userData.json'
 describe('Orange HTM Tests', () => {
 
   const selectorList = {
-    usernameField: "[name='username']",
+    userNameField: "[name='username']",
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
     sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
     dashboardGrid:'.orangehrm-dashboard-grid',
-    wrongCredentialAlert: "[role='alert']"
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericField: '.oxd-input--active',
+    dateField: "[placeholder='yyyy-dd-mm']",
+    backGroundSpace: '.orangehrm-edit-employee-content > :nth-child(1)',
+    submitButton: "[type='submit']",
+    
   }
 
-  it('login - Sucess', () => {
+  it.only('User Info Update - Sucess', () => {
     cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userSucess.username)
+    cy.get(selectorList.userNameField).type(userData.userSucess.username)
     cy.get(selectorList.passwordField).type(userData.userSucess.password)
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorList.dashboardGrid)
+    cy.get(selectorList.myInfoButton).click()
+    cy.get(selectorList.firstNameField).clear().type('NameTest')
+    cy.get(selectorList.lastNameField).clear().type('LastNameTest')
+    cy.get(selectorList.genericField).eq(3).clear().type('Employee')
+    cy.get(selectorList.genericField).eq(4).clear().type('OtherIdTest')
+    cy.get(selectorList.genericField).eq(5).clear().type('DriverTest')
+    cy.get(selectorList.dateField).eq(0).clear().type('2024-12-04')
+    cy.get(selectorList.backGroundSpace).click()
+    cy.get(selectorList.submitButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    
+
+
   })
   it('login - Fail', () => {
     cy.visit('/auth/login')
